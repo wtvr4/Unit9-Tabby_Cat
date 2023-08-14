845e4d5ed48f1e01616ff5a4ef0a2d69e5894429
Mohamed Badir <glury6@gmail.com>
2023-08-14 22:27:50 +0400

    Initial Commit

---
+++ Tabby Cat/Tabby_CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabby_CatApp.swift
+//  Tabby Cat
+//
+//  Created by Mohamed Badir on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tabby_CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

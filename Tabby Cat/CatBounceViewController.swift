1e8000deb9ac78cea572c757d134fe152a71386b
Mohamed Badir <glury6@gmail.com>
2023-08-25 22:28:53 +0400

    0.0.3

---
+++ Tabby Cat/CatBounceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CatBounceViewController.swift
+//  Tabby Cat
+//
+//  Created by Mohamed Badir on 25/08/2023.
+//
+
+import UIKit
+
+class CatBounceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tabby Cat/CatImagesView.swift
+++ Tabby Cat/CatImagesView.swift
@@ -1,11 +1,29 @@
 import SwiftUI
-
+import Subsonic
 struct CatImagesView: View {
+    
+    @State var catURL = "https://cataas.com/cat?q="
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        AsyncImage(url: URL(string: catURL)!) { image in
+            Spacer()
+            image
+                .resizable()
+                .scaledToFit()
+                .onAppear {
+                    play(sound: "meow.m4a")
+                }
+            Spacer()
+            Button("Load Another"){
+            catURL += "1"
+            }
+            .buttonStyle(.borderedProminent)
+        } placeholder: {
+            ProgressView()
+        }
+
     }
 }
-
 struct CatImagesView_Previews: PreviewProvider {
     static var previews: some View {
         CatImagesView()
